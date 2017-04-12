15385348c376dd2848218397c81df017f26b1425
Andrew Chen <yongjhih@gmail.com>
2017-04-12 15:01:11 -0700

    Rename function extensions

--- rxfirebase2-auth-kotlin/src/main/kotlin/rxfirebase2/auth/RxFirebaseUser.kt
+++ rxfirebase2-auth-kotlin/src/main/kotlin/rxfirebase2/auth/RxFirebaseUser.kt
@@ -8,43 +8,43 @@ import com.google.firebase.auth.UserProfileChangeRequest
 import io.reactivex.Completable
 import io.reactivex.Single
 
-inline fun FirebaseUser.rxDelete()
+inline fun FirebaseUser.deletes()
         : Completable
         = RxFirebaseUser.delete(this)
 
-inline fun FirebaseUser.rxGetToken(forceRefresh: Boolean)
+inline fun FirebaseUser.getsToken(forceRefresh: Boolean)
         : Single<String>
         = RxFirebaseUser.getToken(this, forceRefresh)
 
-inline fun FirebaseUser.rxLinkWithCredential(credential: AuthCredential)
+inline fun FirebaseUser.linksWithCredential(credential: AuthCredential)
         : Single<FirebaseUser>
         = RxFirebaseUser.linkWithCredential(this, credential)
 
-inline fun FirebaseUser.rxReauthenticate(credential: AuthCredential)
+inline fun FirebaseUser.reauthenticates(credential: AuthCredential)
         : Completable
         = RxFirebaseUser.reauthenticate(this, credential)
 
-inline fun FirebaseUser.rxReload()
+inline fun FirebaseUser.reloads()
         : Completable
         = RxFirebaseUser.reload(this)
 
-inline fun FirebaseUser.rxSendEmailVerification()
+inline fun FirebaseUser.sendsEmailVerification()
         : Completable
         = RxFirebaseUser.sendEmailVerification(this)
 
-inline fun FirebaseUser.rxUnlink(provider: String)
+inline fun FirebaseUser.unlinks(provider: String)
         : Single<FirebaseUser>
         = RxFirebaseUser.unlink(this, provider)
 
-inline fun FirebaseUser.rxUpdateEmail(email: String)
+inline fun FirebaseUser.updatesEmail(email: String)
         : Completable
         = RxFirebaseUser.updateEmail(this, email)
 
-inline fun FirebaseUser.rxUpdatePassword(password: String)
+inline fun FirebaseUser.updatesPassword(password: String)
         : Completable
         = RxFirebaseUser.updatePassword(this, password)
 
-inline fun FirebaseUser.rxUpdateProfile(request: UserProfileChangeRequest)
+inline fun FirebaseUser.updatesProfile(request: UserProfileChangeRequest)
         : Completable
         = RxFirebaseUser.updateProfile(this, request)
 
